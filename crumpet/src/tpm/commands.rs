aab6859dd383e663ef6cbe745149ad0b1f8600d4
Hitesh Jasani <hitesh.jasani@gmail.com>
2026-08-10 22:21:22 -0400

    Use constants

--- crumpet/src/tpm/commands.rs
+++ crumpet/src/tpm/commands.rs
@@ -267,12 +267,11 @@ pub fn parse_policy_secret_response(resp: &[u8]) -> Result<(), Box<dyn Error>> {
 pub fn build_rsa_signing_template() -> Vec<u8> {
     // objectAttributes: fixedTPM | fixedParent | sensitiveDataOrigin |
     // userWithAuth | sign
-    let attrs: u32 = (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 18);
-    // let attrs: u32 = TPMA_OBJECT_FIXED_TPM
-    //     | TPMA_OBJECT_FIXED_PARENT
-    //     | TPMA_OBJECT_SENSITIVE_DATA_ORIGIN
-    //     | TPMA_OBJECT_USER_WITH_AUTH
-    //     | TPMA_OBJECT_SIGN_ENCRYPT;
+    let attrs: u32 = TPMA_OBJECT_FIXED_TPM
+        | TPMA_OBJECT_FIXED_PARENT
+        | TPMA_OBJECT_SENSITIVE_DATA_ORIGIN
+        | TPMA_OBJECT_USER_WITH_AUTH
+        | TPMA_OBJECT_SIGN_ENCRYPT;
 
     let mut t = Vec::new();
     t.extend_from_slice(&TPM_ALG_RSA.to_be_bytes()); // type
